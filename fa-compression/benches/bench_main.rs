901f3d6f1cd3dbd1eb245bdb529a5ed34c6a6e61
tibvdm <tibovandemoortele@hotmail.com>
2024-04-03 14:01:47 +0200

    add simple benchmarks

---
+++ fa-compression/benches/bench_main.rs
@@ -0,0 +1,27 @@
+use criterion::{criterion_group, criterion_main};
+use fa_compression::{decode::decode, encode::encode};
+
+mod util;
+
+fn encode_benchmark(c: &mut criterion::Criterion) {
+    c.bench_function("encode", |b| {
+        b.iter_batched(
+            || util::generate_decoded_annotations(100), 
+            |annotations| encode(annotations.as_str()), 
+            criterion::BatchSize::SmallInput
+        )
+    });
+}
+
+fn decode_benchmark(c: &mut criterion::Criterion) {
+    c.bench_function("decode", |b| {
+        b.iter_batched(
+            || util::generate_encoded_annotations(100), 
+            |annotations| decode(annotations.as_slice()), 
+            criterion::BatchSize::SmallInput
+        )
+    });
+}
+
+criterion_group!(benches, encode_benchmark, decode_benchmark);
+criterion_main!(benches);
---
+++ fa-compression/benches/util.rs
@@ -0,0 +1,48 @@
+use fa_compression::encode::encode;
+use rand::{rngs::ThreadRng, Rng};
+
+pub fn generate_ipr(random: &mut ThreadRng) -> String {
+    format!("IPR:IPR{:06}", random.gen_range(0 .. 999999))
+}
+
+pub fn generate_go(random: &mut ThreadRng) -> String {
+    format!("GO:{:07}", random.gen_range(0 .. 9999999))
+}
+
+pub fn generate_ec(random: &mut ThreadRng) -> String {
+    format!("EC:{}.{}.{}.{}", random.gen_range(0 .. 8), random.gen_range(0 .. 30), random.gen_range(0 .. 30), random.gen_range(0 .. 300))
+}
+
+pub fn generate_annotation(random: &mut ThreadRng) -> String {
+    match random.gen_range(0 .. 3) {
+        0 => generate_ipr(random),
+        1 => generate_go(random),
+        2 => generate_ec(random),
+        _ => unreachable!(),
+    }
+}
+
+pub fn generate_decoded_annotations(count: usize) -> String {
+    let mut random = rand::thread_rng();
+
+    let mut annotations = String::new();
+    for _ in 0 .. count {
+        annotations.push_str(&generate_annotation(&mut random));
+        annotations.push(';');
+    }
+    annotations.pop();
+    annotations
+}
+
+pub fn generate_encoded_annotations(count: usize) -> Vec<u8> {
+    let mut random = rand::thread_rng();
+
+    let mut annotations = String::new();
+    for _ in 0 .. count {
+        annotations.push_str(&generate_annotation(&mut random));
+        annotations.push(';');
+    }
+    annotations.pop();
+    
+    encode(annotations.as_str())
+}
