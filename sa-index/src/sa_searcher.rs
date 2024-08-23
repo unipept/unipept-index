c86f96dfd7e439e3e0286825f0694bfe5d66b29f
tibvdm <tibovandemoortele@hotmail.com>
2024-08-23 11:42:36 +0200

    improve checks

--- sa-index/src/sa_searcher.rs
+++ sa-index/src/sa_searcher.rs
@@ -344,18 +344,18 @@ impl Searcher {
                     // If the suffix is smaller than the skip factor, we can't check the prefix
                     let checkable_suffix = suffix >= skip;
 
-                    // Check for trypticity if a tryptic search is performed
-                    let is_tryptic = checkable_suffix && tryptic_search && (
-                        self.proteins.input_string[suffix - skip - 1] == b'R' ||
-                            self.proteins.input_string[suffix - skip - 1] == b'K' ||
-                            self.proteins.input_string[suffix - skip - 1] == SEPARATION_CHARACTER
-                    ) && self.proteins.input_string[suffix - skip + search_string.len() + 1] != b'P';
-
-                    // If a tryptic search is performed and the suffix is not tryptic, we can skip
-                    if tryptic_search && !is_tryptic {
-                        sa_index += 1;
-                        continue;
-                    }
+                    // // Check for trypticity if a tryptic search is performed
+                    // let is_tryptic = checkable_suffix && tryptic_search && (
+                    //     self.proteins.input_string[suffix - skip - 1] == b'R' ||
+                    //         self.proteins.input_string[suffix - skip - 1] == b'K' ||
+                    //         self.proteins.input_string[suffix - skip - 1] == SEPARATION_CHARACTER
+                    // ) && self.proteins.input_string[suffix - skip + search_string.len() + 1] != b'P';
+                    //
+                    // // If a tryptic search is performed and the suffix is not tryptic, we can skip
+                    // if tryptic_search && !is_tryptic {
+                    //     sa_index += 1;
+                    //     continue;
+                    // }
 
                     // If the skip factor is 0, the entire search string should match.
                     let completely_matched = checkable_suffix && skip == 0;
