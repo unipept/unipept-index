1cd0465bea8a2c6af74fe39cffe32d8142e0ef66
Simon Van de Vyver <99675815+SimonVandeVyver@users.noreply.github.com>
2024-10-28 11:37:08 +0100

    change link to unipept-libsais to https

--- libsais64-rs/builder.rs
+++ libsais64-rs/builder.rs
@@ -53,7 +53,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // clone the c library
     Command::new("git")
-        .args(["clone", "git@github.com:unipept/unipept-libsais.git", "libsais", "--depth=1"])
+        .args(["clone", "https://github.com/unipept/unipept-libsais.git", "libsais", "--depth=1"])
         .status()
         .expect("Failed to clone the libsais repository");
 
