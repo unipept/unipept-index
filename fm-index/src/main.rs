902e208799ed56b7dfd4c1c09b26913a88ebf54b
Simon Van de Vyver <simon.vandevyver@ugent.be>
2025-07-15 14:10:58 +0200

    report matches per ED, write matches to file

--- fm-index/src/main.rs
+++ fm-index/src/main.rs
@@ -9,6 +9,7 @@ use std::io::{BufRead, Write, self};
 use std::time::Instant;
 use std::fs;
 use std::fs::File;
+use std::env;
 
 static FM_PATH: &str = "../test_files/uniprot";
 static SEARCH_SCHEME_PATH: &str = "../search_schemes/kuch_k+1/1/searches.txt";
@@ -107,45 +108,39 @@ fn parameter_search(fm_index: &FMIndex, patterns: Vec<Vec<u8>>) -> Result<(), Bo
     Ok(())
 }
 
-fn parameter_search_in_text(fm_index: &FMIndex, patterns: Vec<Vec<u8>>) -> Result<HashSet<FMMatch>, Box<dyn Error>> {
+fn parameter_search_in_text(fm_index: &FMIndex, patterns: Vec<Vec<u8>>, ed: i32) -> Result<HashSet<FMMatch>, Box<dyn Error>> {
 
-    let max_ed = 3;
-
-    let mut search_schemes = vec![None];
-    for i in 1..max_ed {
-        let path_str = format!("../search_schemes/kuch_k+1/{}/searches.txt", i);
-        println!("{}", &path_str);
+    let mut search_scheme = None;
+    if ed > 0 {
+        let path_str = format!("../search_schemes/kuch_k+1/{}/searches.txt", ed);
         let searches_path = Path::new(&path_str);
-        search_schemes.push(Some(SearchScheme::from_file(searches_path)?));
+        search_scheme = Some(SearchScheme::from_file(searches_path)?);
     }
 
-    let mut matches: HashSet<FMMatch> = HashSet::new();
+    let patterns: Vec<Vec<u8>> = patterns
+        .iter()
+        .cloned()
+        .filter(|v| v.len() >= 7 + ed as usize)
+        .collect();
 
-    for (i, search_scheme) in search_schemes[0..max_ed].iter().enumerate() {
-
-        let patterns: Vec<Vec<u8>> = patterns
-            .iter()
-            .cloned()
-            .filter(|v| v.len() >= 7+i)
-            .collect();
-
-        eprintln!("Start searching for ED {}...", i);
-        let start = Instant::now();
-        let new_matches = match search_scheme {
-            Some(scheme) => search_multiple(fm_index, patterns.clone(), &scheme).map_err(|e| e as Box<dyn Error>)?,
-            None => search_multiple_exact(fm_index, patterns.clone()).map_err(|e| e as Box<dyn Error>)?
-        };
-        matches.extend(new_matches);
-        
-        let duration = start.elapsed();
-        eprintln!("Searching done in {:?}", duration);
-    }
+    eprintln!("Start searching for ED {}...", ed);
+    let start = Instant::now();
+    let matches = match search_scheme {
+        Some(scheme) => search_multiple(fm_index, patterns.clone(), &scheme).map_err(|e| e as Box<dyn Error>)?,
+        None => search_multiple_exact(fm_index, patterns.clone()).map_err(|e| e as Box<dyn Error>)?
+    };
+    
+    let duration = start.elapsed();
+    eprintln!("Searching done in {:?}", duration);
 
     Ok(matches)
 }
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+    let args: Vec<String> = env::args().collect();
+    let output_base = &args[1];
 
     let base_path = Path::new(FM_PATH);
     eprintln!("Start loading index...");
@@ -157,21 +152,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|line| Ok(line?.into_bytes()))
         .collect::<Result<Vec<_>, Box<dyn Error>>>()?;
 
-    let matches = parameter_search_in_text(&fm_index, patterns)?;
+    let max_ed=2;
+    let mut matches_per_ed: Vec<HashSet<FMMatch>> = Vec::new();
+    for ed in 0..=max_ed {
+        matches_per_ed.push(parameter_search_in_text(&fm_index, patterns.clone(), ed)?);
+    }
+    
     drop(fm_index);
 
-    eprintln!("Reporting matches...");
-    let mut writer = io::stdout();
-    let input_path = Path::new(TEXT_PATH);
-    let text = String::from_utf8(fs::read(input_path)?)?;
+    for ed in 0..=max_ed {
+        eprintln!("Reporting matches for edit distance {}...", ed);
+        let output_path = format!("{}_ed{}.txt", output_base, ed);
+        let output_path = Path::new(&output_path);
+        let mut writer = File::create(output_path)?;
+        let input_path = Path::new(TEXT_PATH);
+        let text = String::from_utf8(fs::read(input_path)?)?;
 
-    let mut sorted_matches: Vec<_> = matches.into_iter().collect();
-    sorted_matches.sort_by_key(|m| m.start_position);
+        let mut sorted_matches: Vec<_> = matches_per_ed[ed as usize].clone().into_iter().collect();
+        sorted_matches.sort_by_key(|m| m.start_position);
 
-    for m in sorted_matches {
-        let end = m.start_position + m.length;
-        writeln!(writer, "{}", &text[m.start_position..end])?;
+        for m in sorted_matches {
+            let end = m.start_position + m.length;
+            writeln!(writer, "{}", &text[m.start_position..end])?;
+        }
     }
+    
 
     Ok(())
 }
