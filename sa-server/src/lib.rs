97b4dca2a5fff55cef73a258e9b2531d8736f025
tibvdm <tibovandemoortele@hotmail.com>
2026-03-11 17:32:04 +0100

    remove debug lines

--- sa-server/src/lib.rs
+++ sa-server/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use std::fs::{File, OpenOptions};
-use std::io::{BufReader, BufWriter, Read};
+use std::fs::File;
+use std::io::{BufReader, Read};
 use sa_compression::load_compressed_suffix_array;
 use sa_index::binary::{load_suffix_array, load_suffix_array_mmap};
 use sa_index::SuffixArray;
@@ -28,23 +28,5 @@ pub fn load_suffix_array_file(file: &str, use_mmap: bool) -> Result<SuffixArray,
 }
 
 pub fn load_proteins_file(file: &str, use_mmap: bool) -> Result<Proteins, Box<dyn Error>> {
-    if use_mmap {
-        Proteins::try_from_binary_file(&file, true)
-    } else {
-        let proteins_filename = "/mnt/data/uniprot-2025-04/suffix-array/proteins.bin";
-        let proteins = Proteins::try_from_database_file(&file)?;
-        let mut proteins_file = open_file_buffer(&proteins_filename, 100 * 1024 * 1024)?;
-        proteins.write_binary(&mut proteins_file)?;
-        Ok(proteins)
-    }
-}
-
-fn open_file_buffer(file: &str, buffer_size: usize) -> std::io::Result<BufWriter<File>> {
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true) // if the file already exists, empty the file
-        .open(file)?;
-
-    Ok(BufWriter::with_capacity(buffer_size, file))
+    Proteins::try_from_binary_file(&file, use_mmap)
 }
